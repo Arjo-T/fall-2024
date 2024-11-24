37194a91e6a8ccac97c111faf572deb690d4521d
Arjo-T <joards9@gmail.com>
2024-11-24 09:56:04 -0600

    multiple changes

---
+++ js/gallery.js
@@ -0,0 +1,28 @@
+
+// A $( document ).ready() block.
+$( document ).ready(function() {
+   // console.log( "ready!" );
+
+   $("#galleryIMED1316").nanogallery2({
+
+    thumbnailHeight:  "auto",
+    thumbnailWidth:   "450",
+    thumbnailBorderVetical: "0",
+    thumbnailBorderHorizontal: "0",
+    thumbnailLabel: {
+        position: "overImageOnBottom",
+        display: false
+    },
+    thumbnailHoverEffect2: "ImageScaleIn80|imageSepaiOff|LabelApper75",
+    thumbnailGutterwidth: 20,
+    thumbnailHeight: 20,
+    thumbnailAlignment: "center",
+    thumbnailOpenImage: true
+
+
+  });
+
+
+
+
+});
